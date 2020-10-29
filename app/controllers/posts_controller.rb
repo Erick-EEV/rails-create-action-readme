e4e8243fee8c59178d38e26affa06d25e73fad13
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-29 19:28:29 -0400

    I LEARNED

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,17 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    post_title = params[:title]
+    post_desc = params[:description]
+    new_post = Post.create(title: post_title, description: post_desc)
+
+    redirect_to post_path(new_post.id)
+
+    # post = Post.new
+    # post.title = params[:title]
+    # post.description = params[:description]
+    # post.save
+  end
   # add create method here
 end
